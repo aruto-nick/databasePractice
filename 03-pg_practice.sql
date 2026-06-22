92f5b8065ad2347c597c1951204fda1e46c72434
aruto-nick <effo.conti329@gmail.com>
2026-06-22 17:19:41 +0900

    third commit

--- 03-pg_practice.sql
+++ 03-pg_practice.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE practice;
 
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
-use practice;
+\c practice;
 create table users(id int, name varchar(10));
 
 -- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。

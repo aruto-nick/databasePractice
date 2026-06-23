7b0849ac494df606907c62d57f07e79ce6bbd79d
aruto-nick <effo.conti329@gmail.com>
2026-06-23 11:53:17 +0900

    5th commit

--- 03-pg_practice.sql
+++ 03-pg_practice.sql
@@ -3,22 +3,17 @@ CREATE DATABASE practice;
 
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
 \c practice;
-create table users(id SERIAL, name TEXT, age SMALLINT, gender TEXT);
-INSERT INTO jobs(id, name, age, gender)VALUES
-(1, '大谷 翔平', 29, 'Man'),
-(2, 'ヒカキン', 34, 'Man'),
-(3, '吉岡 里帆', 31,  'Woman'),
-(4, '本田 翼', 31, 'Woman'),
-(5, '八村 塁', 25, 'Man'),
-(6, '深田 恭子', 41, 'Woman');
+create table users(
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "name" CHARACTER(255) NOT NULL DEFAULT "" COMMENT "氏名",
+    "age" INTEGER NOT NULL DEFAULT 0 COMMENT "年齢",
+    "gender" ENUM("Man", "Woman", "Other") NOT NULL DEFAULT "Other" COMMENT "性別", )
+
 
 -- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
-create table jobs(id SERIAL, user-id SERIAL,name TEXT, gender TEXT);
-INSERT INTO jobs(id, name, age, gender)VALUES
-(1, 1, 'プロ野球選手'),
-(2, 2, 'YouTuber'),
-(3, 3, '女優'),
-(4, 4, '女優'),
-(5, 4, 'ファッションモデル'),
-(6, 5, 'プロバスケットボール選手'),
-(7, 6, '女優');
+create table jobs(
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "user-id" INTEGER NOT NULL,
+    "name" CHARACTER(255) NOT NULL DEFAULT "会社員" COMMENT "仕事名",
+    FOREIGN KEY "fk_user_id" ("user_id") REFERENCES "users"("id")
+    );
